2476b82d5369654bf5aca1b23c9afa04fa41efac
Yuzhe Jin <827498681@qq.com>
2024-05-06 05:31:12 +0800

    test for more versions

--- test_z3.py
+++ test_z3.py
@@ -6,11 +6,7 @@ csv_path = "/home/uu613/workspace/bugs/new_folder/z3_bugs.csv"
 tested_csv_path = "/home/uu613/workspace/bugs/new_folder/tested_z3_bugs.csv"
 basic_path = "/home/uu613/workspace/z3_versions/"
 case_path = "/home/uu613/workspace/bugs/new_folder/"
-versions = ['z3-4.7.1-x64-ubuntu-16.04',
-            'z3-4.8.1.016872a5e0f6-x64-ubuntu-16.04',
-            'z3-4.8.3.7f5d66c3c299-x64-ubuntu-16.04',
-            'z3-4.8.4.d6df51951f4c-x64-ubuntu-16.04',
-            'z3-4.8.5-x64-ubuntu-16.04']
+versions = [str(version) for version in os.listdir(basic_path) if version.startswith('z3-')]
 
 
 def mark_buggy_version():
@@ -47,6 +43,8 @@ if __name__ == '__main__':
     for version in versions:
         df[version] = ''
         z3_path = basic_path + version + '/bin/z3'
+        # 授权
+        os.chmod(z3_path, 0o755)
         for index, row in df.iterrows():
             case_filename = row['Case-Filename']
             expected_result = row['result']
